f9a96ad44cd600a9babc27a453d64979f75f9191
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-09-04 00:54:05 +0800

    BFS magic, both O(V+E), Like

--- HackerRank/The HackerRank Interview Preparation Kit/Graphs/Find the nearest clone.cpp
+++ HackerRank/The HackerRank Interview Preparation Kit/Graphs/Find the nearest clone.cpp
@@ -43,3 +43,43 @@ int findShortest(int graph_nodes, vector<int> graph_from, vector<int> graph_to, 
 
     return mn == 1e9 ? -1 : mn;
 }
+
+// One bfs. Optimal, use map to record. when counter in map then merge.
+// and their parent must not same. Nice. O(V+E) faster.
+
+int findShortest(int graph_nodes, vector<int> graph_from, vector<int> graph_to, vector<long> ids, int val) {
+    // solve here
+    vector<int> g[graph_nodes+1];
+    int m = graph_from.size();
+
+    for(int i = 0; i < m; ++i){
+        g[graph_from[i]].push_back(graph_to[i]);
+        g[graph_to[i]].push_back(graph_from[i]);
+    }
+
+    unordered_map<int, pair<int, int>> mp;
+    queue<int> q;
+    for(int i = 1; i <= graph_nodes; ++i){
+        if(ids[i-1] == val){
+            mp[i] = {i, 0};
+            q.push(i);
+        }
+    }
+
+    while(q.size()){
+        int u = q.front(); q.pop();
+        auto [par, len] = mp[u];
+        for(int v: g[u]){
+            if(!mp.count(v)){
+                mp[v] = {par, len+1};
+                q.push(v);
+            }
+            else{
+                if(mp[v].first == par) continue;
+                return len + mp[v].second + 1;
+            }
+        }
+    }
+
+    return -1;
+}
