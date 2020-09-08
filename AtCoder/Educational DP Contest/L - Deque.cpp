a98753b62c38e4a921f7d283e52a9b4511d8a1a6
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-09-08 16:03:18 +0800

    Dp, Minimax

--- AtCoder/Educational DP Contest/L - Deque.cpp
+++ AtCoder/Educational DP Contest/L - Deque.cpp
@@ -53,3 +53,52 @@ int main()
     //}
     return 0;
 }
+
+// Other. Optimal.
+#include <bits/stdc++.h>
+
+#define Fast cin.tie(0), ios::sync_with_stdio(0)
+#define All(x) x.begin(), x.end()
+#define louisfghbvc int t; cin >> t; while(t--)
+#define sz(x) (int)(x).size()
+using namespace std;
+typedef long long LL;
+typedef pair<LL, LL> ii;
+typedef vector<LL> vi;
+
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '{'; string sep = ""; for(const auto &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+
+void dbg_out() { cerr << "\n"; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+const int N = 3e3+5;
+LL arr[N];
+LL dp[N][N];
+
+LL dfs(int l, int r){
+    if(l > r) return 0;
+    if(dp[l][r] != -1) return dp[l][r];
+    LL res = max(arr[l] - dfs(l+1, r), arr[r] - dfs(l, r-1));
+    return dp[l][r] = res;
+}
+
+void solve(){
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; ++i) cin >> arr[i];
+
+    memset(dp, -1, sizeof dp);
+    cout << dfs(0, n-1) << "\n";
+}
+
+int main()
+{
+    Fast;
+    //louisfghbvc{
+        solve();
+    //}
+    return 0;
+}
+
+
