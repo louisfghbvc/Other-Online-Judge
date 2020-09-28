e650153fa0c5c43ba514e3191e6713b643bd408b
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-09-28 20:04:07 +0800

    Number theory.

---
+++ CodeForces/Round #674 (Div. 3)/C - Increase and Copy.cpp
@@ -0,0 +1,43 @@
+// Enum all case. from 1 to sqrt(N). just like factor. the more value, copy fast.
+// so enum all val. and why not value could be fusion? observe the ans...
+// O(sqrt(N)).
+
+#include <bits/stdc++.h>
+ 
+#define Fast cin.tie(0), ios::sync_with_stdio(0)
+#define All(x) x.begin(), x.end()
+#define louisfghbvc int t; cin >> t; while(t--)
+#define sz(x) (int)(x).size()
+using namespace std;
+typedef long long LL;
+typedef pair<LL, LL> ii;
+typedef vector<LL> vi;
+ 
+template<typename T> istream& operator>>(istream &is, vector<T> &v) { for(auto &it : v) is >> it; return is; }
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '{'; string sep = ""; for(const auto &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+ 
+void dbg_out() { cerr << "\n"; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+ 
+const int N = 2e5+5;
+ 
+void solve(){
+    int n;
+    cin >> n;
+    LL res = n;
+    for(LL i = 1; i*i <= n; ++i){
+        LL addC = i-1;
+        LL copyC = (n-i+(i-1))/i;
+        res = min(res, addC + copyC);
+    }
+    cout << res << "\n";
+}
+ 
+int main()
+{
+    Fast;
+    louisfghbvc
+        solve();
+    return 0;
+}
