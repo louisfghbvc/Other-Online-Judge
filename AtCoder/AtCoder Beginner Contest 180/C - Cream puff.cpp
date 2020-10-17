4cc4847e265d7bb34e7ad6aab3e8ef1b6a1953ab
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-10-17 22:05:23 +0800

    Prime. Simple. O(sqrt(N))

---
+++ AtCoder/AtCoder Beginner Contest 180/C - Cream puff.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+#define Fast cin.tie(0), ios::sync_with_stdio(0)
+#define All(x) x.begin(), x.end()
+#define louisfghbvc int t; cin >> t; while(t--)
+#define sz(x) (int)(x).size()
+using namespace std;
+typedef long long LL;
+typedef pair<LL, LL> ii;
+typedef vector<LL> vi;
+
+template<typename T> istream& operator>>(istream &is, vector<T> &v) { for(auto &it : v) is >> it; return is; }
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '{'; string sep = ""; for(const auto &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+
+void dbg_out() { cerr << " end.\n"; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+
+void solve(){
+    LL n;
+    cin >> n;
+    set<LL> arr;
+    for(LL i = 1; i*i <= n; i++){
+        if(n % i == 0) arr.insert(i), arr.insert(n/i);
+    }
+    for(LL x: arr)
+        cout << x << "\n";
+}
+
+int main()
+{
+    Fast;
+    //louisfghbvc
+        solve();
+    return 0;
+}
