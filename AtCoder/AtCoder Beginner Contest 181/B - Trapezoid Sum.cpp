ea1c515a8b5abd84ea109a7435bea14cecd4aa1e
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-11-01 23:00:18 +0800

    Sum. formula

---
+++ AtCoder/AtCoder Beginner Contest 181/B - Trapezoid Sum.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+#define Fast cin.tie(0), ios::sync_with_stdio(0)
+#define All(x) x.begin(), x.end()
+#define louisfghbvc int t; cin >> t; while(t--)
+#define sz(x) (int)(x).size()
+using namespace std;
+typedef long long LL;
+typedef pair<LL, LL> ii;
+typedef vector<LL> vi;
+
+template<typename T> istream& operator>>(istream &is, vector<T> &v) { for(auto &it : v) is >> it; return is; }
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '{'; string sep = ""; for(const auto &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+
+void dbg_out() { cerr << " end.\n"; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+const int N = 2e5+5;
+
+void solve(){
+    int n;
+    cin >> n;
+    LL a, b;
+    LL res = 0;
+    for(int i = 0; i < n; ++i){
+        cin >> a >> b;
+        res += (a+b)*(b-a+1)/2;
+    }
+    cout << res << "\n";
+}
+
+int main()
+{
+    Fast;
+    //louisfghbvc
+        solve();
+    return 0;
+}
