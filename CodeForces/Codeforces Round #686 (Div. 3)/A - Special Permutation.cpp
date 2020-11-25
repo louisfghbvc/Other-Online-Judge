def204486365191b271c911034b100c386e7c8a0
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-11-25 08:55:53 +0800

    Rotate 1.

---
+++ CodeForces/Codeforces Round #686 (Div. 3)/A - Special Permutation.cpp
@@ -0,0 +1,40 @@
+// Cool. Just rotate 1.
+// O(n).
+
+#include <bits/stdc++.h>
+
+#define Fast cin.tie(0), ios::sync_with_stdio(0)
+#define All(x) x.begin(), x.end()
+#define louisfghbvc int t; cin >> t; while(t--)
+#define sz(x) (int)(x).size()
+using namespace std;
+typedef long long LL;
+typedef pair<LL, LL> ii;
+typedef vector<LL> vi;
+
+template<typename T> istream& operator>>(istream &is, vector<T> &v) { for(auto &it : v) is >> it; return is; }
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '{'; string sep = ""; for(const auto &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+
+void dbg_out() { cerr << " end.\n"; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+const int N = 100+5;
+
+
+void solve(){
+    int n;
+    cin >> n;
+    vi arr(n);
+    for(int i = 0; i < n; ++i) arr[i] = (i+1)%n+1;
+    for(int i =  0; i < n; ++i)
+        cout << arr[i] << " \n"[i==n-1];
+}
+
+int main()
+{
+    Fast;
+    louisfghbvc
+        solve();
+    return 0;
+}
