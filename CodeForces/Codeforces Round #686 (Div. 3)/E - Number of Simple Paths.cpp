e5dc46524924ec3a981c4dd5891ba66197ae0f31
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2020-12-31 11:38:23 +0800

    O(N) DFS/BFS Tree Cycle Math.

--- CodeForces/Codeforces Round #686 (Div. 3)/E - Number of Simple Paths.cpp
+++ CodeForces/Codeforces Round #686 (Div. 3)/E - Number of Simple Paths.cpp
@@ -84,3 +84,81 @@ int main()
         solve();
     return 0;
 }
+
+
+// O(N). BFS. So from outer to inner. topology
+#include <bits/stdc++.h>
+
+#define Fast cin.tie(0), ios::sync_with_stdio(0)
+#define All(x) x.begin(), x.end()
+#define louisfghbvc int t; cin >> t; while(t--)
+#define sz(x) (int)(x).size()
+using namespace std;
+typedef long long LL;
+typedef pair<LL, LL> ii;
+typedef vector<int> vi;
+
+template<typename T> istream& operator>>(istream &is, vector<T> &v) { for(auto &it : v) is >> it; return is; }
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '{'; string sep = ""; for(const auto &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+
+void dbg_out() { cerr << " end.\n"; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+const int N = 2e5+5;
+vi g[N];
+bool ncyc[N];
+int ind[N];
+
+int dfs(int u, int p = -1){
+    int res = 1;
+    for(int v: g[u]){
+        if(v == p || !ncyc[v]) continue;
+        res += dfs(v, u);
+    }
+    return res;
+}
+
+void solve(){
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; ++i) g[i].clear();
+    memset(ncyc, 0, sizeof ncyc);
+    memset(ind, 0, sizeof ind);
+
+    for(int i = 0; i < n; ++i){
+        int a, b;
+        cin >> a >> b;
+        --a, --b;
+        g[a].push_back(b);
+        g[b].push_back(a);
+        ind[a]++, ind[b]++;
+    }
+
+    queue<int> q;
+    for(int i = 0; i < n; ++i) if(ind[i] == 1) q.push(i);
+    while(q.size()){
+        int u = q.front(); q.pop();
+        ncyc[u] = 1;
+        for(int v: g[u]){
+            if(--ind[v] == 1) q.push(v);
+        }
+    }
+
+    LL res = 1LL * n * (n-1);
+    for(int i = 0; i < n; ++i){
+        if(ncyc[i]) continue;
+        int Size = dfs(i);
+        res -= 1LL * Size * (Size-1) / 2;
+    }
+    cout << res << "\n";
+}
+
+int main()
+{
+    Fast;
+    louisfghbvc
+        solve();
+    return 0;
+}
