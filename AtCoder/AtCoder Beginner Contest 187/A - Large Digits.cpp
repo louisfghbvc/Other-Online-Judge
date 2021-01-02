fc3a466bad09234fe1f466baa8ca7826eac15c10
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-01-02 23:21:44 +0800

    Simple.

---
+++ AtCoder/AtCoder Beginner Contest 187/A - Large Digits.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+#define Fast cin.tie(0), ios::sync_with_stdio(0)
+#define All(x) x.begin(), x.end()
+#define louisfghbvc int t; cin >> t; while(t--)
+#define sz(x) (int)(x).size()
+using namespace std;
+typedef long long LL;
+typedef pair<LL, LL> ii;
+typedef vector<LL> vi;
+
+template<typename T> istream& operator>>(istream &is, vector<T> &v) { for(auto &it : v) is >> it; return is; }
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '{'; string sep = ""; for(const auto &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+
+void dbg_out() { cerr << " end.\n"; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+const int N = 5e5+5;
+const int P = 60;
+const int mod = 1e9+7;
+
+void solve(){
+    string a, b;
+    cin >> a >> b;
+    cout << max(a[0]-'0' + a[1] - '0' + a[2] - '0', b[0]-'0' + b[1] - '0' + b[2] - '0') << "\n";
+}
+
+int main()
+{
+    Fast;
+    //louisfghbvc
+        solve();
+    return 0;
+}
