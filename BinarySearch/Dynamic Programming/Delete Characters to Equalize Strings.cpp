f74d731f2716cb7b46d8f5409dc527e556d5daf5
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-01-23 17:38:14 +0800

    LCS.

---
+++ BinarySearch/Dynamic Programming/Delete Characters to Equalize Strings.cpp
@@ -0,0 +1,17 @@
+// O(NM)
+
+int solve(string a, string b) {
+    int n = a.size(), m = b.size();
+
+    int dp[n+1][m+1];
+    memset(dp, 0, sizeof dp);
+
+    for(int i = 1; i <= n; ++i){
+        for(int j = 1; j <= m; ++j){
+            if(a[i-1] == b[j-1]) dp[i][j] = dp[i-1][j-1]+1;
+            else dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
+        }
+    }
+
+    return n+m - 2*dp[n][m];
+}
