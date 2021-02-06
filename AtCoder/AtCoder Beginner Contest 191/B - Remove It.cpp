c5b42ce9ed0a62d0521c311d091cc56132de43a6
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-02-06 21:50:48 +0800

    Simple

---
+++ AtCoder/AtCoder Beginner Contest 191/B - Remove It.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+#define Fast cin.tie(0), ios::sync_with_stdio(0)
+#define All(x) x.begin(), x.end()
+#define louisfghbvc int t; cin >> t; while(t--)
+#define sz(x) (int)(x).size()
+using namespace std;
+typedef long long LL;
+typedef pair<LL, LL> ii;
+typedef vector<LL> vi;
+
+template<typename T> istream& operator>>(istream &is, vector<T> &v) { for(auto &it : v) is >> it; return is; }
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '{'; string sep = ""; for(const auto &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+
+void dbg_out() { cerr << " end.\n"; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+const int N = 3e5+5;
+const int mod = 1e9+7;
+/**
+Read problem statement carefully
+**/
+
+
+void solve(){
+    int n, x;
+    cin >> n >> x;
+    vi arr;
+    for(int i = 0, v; i < n; ++i){
+        cin >> v;
+        if(v != x) arr.push_back(v);
+    }
+    for(int i = 0; i < sz(arr); ++i)
+        cout << arr[i] << " \n"[i+1==sz(arr)];
+}
+
+int main()
+{
+    Fast;
+    //louisfghbvc
+        solve();
+    return 0;
+}
+
+/**
+enjoy the problem.
+**/
