cbfefaa4d478c403b967834a05e98b7f96fef380
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-02-27 21:51:38 +0800

    Observe.

---
+++ AtCoder/A - Discount.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+#define Fast cin.tie(0), ios::sync_with_stdio(0)
+#define All(x) x.begin(), x.end()
+#define louisfghbvc int t; cin >> t; while(t--)
+#define sz(x) (int)(x).size()
+using namespace std;
+typedef long long LL;
+typedef pair<LL, LL> ii;
+typedef vector<LL> vi;
+
+template<typename T> istream& operator>>(istream &is, vector<T> &v) { for(auto &it : v) is >> it; return is; }
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '{'; string sep = ""; for(const auto &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+
+void dbg_out() { cerr << " end.\n"; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+const int N = 1e2+5;
+const int inf = 0x3f3f3f3f;
+/**
+Read problem statement carefully
+sum + len*y == x
+**/
+
+
+
+void solve(){
+    LL a, b;
+    cin >> a >> b;
+    printf("%.12f\n", (double)(a-b)/a * 100);
+}
+
+int main()
+{
+    Fast;
+    //louisfghbvc
+        solve();
+    return 0;
+}
+
+/**
+Enjoy the problem.
+**/
