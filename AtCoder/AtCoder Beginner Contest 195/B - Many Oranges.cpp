2ddf4bb96524326141ffaa6027e5585076446762
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-03-14 09:38:22 +0800

    dp. math

---
+++ AtCoder/AtCoder Beginner Contest 195/B - Many Oranges.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+
+#define Fast cin.tie(0), ios::sync_with_stdio(0)
+#define All(x) x.begin(), x.end()
+#define louisfghbvc int t; cin >> t; while(t--)
+#define sz(x) (int)(x).size()
+using namespace std;
+typedef long long LL;
+typedef pair<LL, LL> ii;
+typedef vector<LL> vi;
+
+template<typename T> istream& operator>>(istream &is, vector<T> &v) { for(auto &it : v) is >> it; return is; }
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '{'; string sep = ""; for(const auto &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+
+void dbg_out() { cerr << " end.\n"; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+const int N = 1500000;
+const int inf = 0x3f3f3f3f;
+/**
+Read problem statement carefully
+sum + len*y == w
+**/
+
+void solve(){
+    int a, b, w;
+    cin >> a >> b >> w;
+    w *= 1000;
+
+    LL mx = -1, mn = -1;
+    if(w % a == 0){
+        mx = w / a;
+    }
+    else{
+        vi dp(w+1, -1);
+        dp[0] = 0;
+        for(LL i = a; i <= b; ++i){
+            for(int j = 0; j+i <= w; ++j){
+                dp[j+i] = max(dp[j+i], dp[j]+1);
+            }
+        }
+        mx = dp[w];
+    }
+    if(w % b == 0){
+        mn = w / b;
+    }
+    else{
+        vi dp(w+1, 1e9);
+        dp[0] = 0;
+        for(LL i = a; i <= b; ++i){
+            for(int j = 0; j+i <= w; ++j){
+                dp[j+i] = min(dp[j+i], dp[j]+1);
+            }
+        }
+        mn = (dp[w] == 1e9) ? -1: dp[w];
+    }
+
+    if(mx >= 0 && mn >= 0){
+        cout << mn << " " << mx << "\n";
+        return;
+    }
+    cout << "UNSATISFIABLE\n";
+}
+
+int main()
+{
+    Fast;
+    //louisfghbvc
+        solve();
+    return 0;
+}
+
+/**
+Enjoy the problem.
+**/
