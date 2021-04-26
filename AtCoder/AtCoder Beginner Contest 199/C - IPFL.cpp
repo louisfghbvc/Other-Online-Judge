6dfb89010b02c01a62c0f7cb82fbe902fc8b7a82
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-04-26 23:50:20 +0800

    Offline change

---
+++ AtCoder/AtCoder Beginner Contest 199/C - IPFL.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+#define Fast cin.tie(0), ios::sync_with_stdio(0)
+#define All(x) x.begin(), x.end()
+#define louisfghbvc int t; cin >> t; for(int tt = 0; tt < t; ++tt)
+#define sz(x) (int)(x).size()
+#define sort_unique(x) sort(x.begin(), x.end()); x.erase(unique(x.begin(), x.end()));
+using namespace std;
+typedef long long LL;
+typedef pair<LL, LL> ii;
+typedef vector<LL> vi;
+
+template<typename T> istream& operator>>(istream &is, vector<T> &v) { for(auto &it : v) is >> it; return is; }
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '{'; string sep = ""; for(const auto &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+
+void dbg_out() { cerr << " end.\n"; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+const int N = 3e5+5;
+const int INF = 0x3f3f3f3f;
+/**
+Read problem statement carefully
+**/
+
+int n, q;
+int get(int x){
+    if(x > n){
+        x -= n;
+    }
+    else
+        x += n;
+    return x;
+}
+
+void solve(int x){
+
+    string s;
+
+    cin >> n;
+    cin >> s;
+
+    cin >> q;
+    int t, a, b;
+    bool sw = 0;
+    while(q--){
+        cin >> t >> a >> b;
+        if(t == 1){
+            if(sw) swap(s[get(a)-1], s[get(b)-1]);
+            else swap(s[a-1], s[b-1]);
+        }
+        else{
+            sw ^= 1;
+        }
+    }
+    if(sw){
+        for(int i = 0; i < n; ++i)
+            swap(s[i], s[i+n]);
+    }
+    cout << s << "\n";
+}
+
+int main()
+{
+    Fast;
+    //louisfghbvc
+        solve(1);
+    return 0;
+}
+
+/**
+Enjoy the problem.
+**/
