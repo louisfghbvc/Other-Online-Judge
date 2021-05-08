56fe0e595397923653ccafe4210750f90b1ff26a
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-05-08 22:05:10 +0800

    Simple math

---
+++ AtCoder/AtCoder Beginner Contest 200/B - 200th ABC-200.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+#define Fast cin.tie(0), ios::sync_with_stdio(0)
+#define All(x) x.begin(), x.end()
+#define louisfghbvc int t; cin >> t; for(int tt = 0; tt < t; ++tt)
+#define sz(x) (int)(x).size()
+#define sort_unique(x) sort(x.begin(), x.end()); x.erase(unique(x.begin(), x.end()));
+using namespace std;
+typedef long long LL;
+typedef pair<LL, LL> ii;
+typedef vector<LL> vi;
+
+template<typename T> istream& operator>>(istream &is, vector<T> &v) { for(auto &it : v) is >> it; return is; }
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '{'; string sep = ""; for(const auto &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+
+void dbg_out() { cerr << " end.\n"; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+const int N = 5e3+5;
+const int INF = 0x3f3f3f3f;
+/**
+Read problem statement carefully
+
+**/
+
+
+void solve(LL n){
+    int k;
+    cin >> n >> k;
+    for(int i = 0; i < k ;++i){
+        if(n % 200 == 0) n /= 200;
+        else n = n*1000 + 200;
+    }
+    cout << n << "\n";
+}
+
+int main()
+{
+    // Fast;
+    // freopen("in.txt", "r", stdin);
+    // freopen("out.txt", "w", stdout);
+    // louisfghbvc
+        solve(1);
+    return 0;
+
+}
+
+/**
+Enjoy the problem.
+**/
