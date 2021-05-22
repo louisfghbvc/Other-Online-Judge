43639a82e8179d5d13fee616d8807b0f9750c45f
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-05-22 12:27:52 +0800

    Binary Search.

---
+++ CSES/Sorting and Searching/Factory Machines.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+#define Fast cin.tie(0), ios::sync_with_stdio(0)
+#define All(x) x.begin(), x.end()
+#define louisfghbvc int t; cin >> t; for(int tt = 0; tt < t; ++tt)
+#define sz(x) (int)(x).size()
+#define sort_unique(x) sort(x.begin(), x.end()); x.erase(unique(x.begin(), x.end()));
+using namespace std;
+typedef long long LL;
+typedef pair<LL, LL> ii;
+typedef vector<LL> vi;
+
+template<typename T> istream& operator>>(istream &is, vector<T> &v) { for(auto &it : v) is >> it; return is; }
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '{'; string sep = ""; for(const auto &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+
+void dbg_out() { cerr << " end.\n"; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+const int N = 2e5 + 5;
+const int INF = 0x3f3f3f3f;
+const int mod = 1e9+7;
+/**
+Read problem statement carefully
+3 7
+3 2 5
+**/
+
+
+void solve(int n){
+    int t;
+    cin >> n >> t;
+    vi arr(n);
+    cin >> arr;
+    
+    LL l = 1, r = 1e18;
+
+    auto produce = [&](LL time){
+        LL res = 0;
+        for(LL v: arr){
+            res += time / v;
+            if(res >= t) break;
+        }
+        return res;
+    };
+
+    while(l < r){
+        LL mid = l + (r-l) / 2;
+        if(produce(mid) >= t)
+            r = mid;
+        else l = mid+1;
+    }
+
+    cout << l << "\n";
+}
+
+int main()
+{
+    Fast;
+    // freopen("in.txt", "r", stdin);
+    // freopen("out.txt", "w", stdout);
+    // louisfghbvc
+        solve(1);
+    return 0;
+}
+
+/**
+Enjoy the problem.
+**/
