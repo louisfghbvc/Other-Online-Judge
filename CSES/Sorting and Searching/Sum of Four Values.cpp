8a0d0d74d25ac601018112d6b63f7195fd6a6624
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-05-23 23:00:11 +0800

    Tricky. Hashtable.

---
+++ CSES/Sorting and Searching/Sum of Four Values.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+
+#define Fast cin.tie(0), ios::sync_with_stdio(0)
+#define All(x) x.begin(), x.end()
+#define louisfghbvc int t; cin >> t; for(int tt = 0; tt < t; ++tt)
+#define sz(x) (int)(x).size()
+#define sort_unique(x) sort(x.begin(), x.end()); x.erase(unique(x.begin(), x.end()));
+using namespace std;
+typedef long long LL;
+typedef pair<LL, LL> ii;
+typedef vector<LL> vi;
+
+template<typename T> istream& operator>>(istream &is, vector<T> &v) { for(auto &it : v) is >> it; return is; }
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '{'; string sep = ""; for(const auto &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+
+void dbg_out() { cerr << " end.\n"; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+const int N = 2e5 + 5;
+const int INF = 0x3f3f3f3f;
+const int mod = 1e9+7;
+/**
+Read problem statement carefully
+8 15
+3 2 5 8 1 3 2 3
+
+**/
+
+void solve(int n){
+    LL t;
+    cin >> n >> t;
+    vi arr(n);
+    cin >> arr;
+
+    map<LL, ii> mp;
+    for(int i = 0; i < n; ++i){
+        for(int j = i+1; j < n; ++j){
+            mp[arr[i] + arr[j]] = {i, j};
+        }
+    }
+
+    for(int i = 0; i < n; ++i){
+        for(int j = i+1; j < n; ++j){
+            LL sum = arr[i] + arr[j];
+            if(mp.count(t - sum)){
+                auto [a, b] = mp[t-sum];
+                if(a != i && a != j && b != i && b != j){
+                    cout << a+1 << " " << b+1 << " " << i+1 << " " << j+1 << "\n";
+                    return;
+                }
+            }
+        }
+    }
+
+    cout << "IMPOSSIBLE\n";
+}
+
+int main()
+{
+    Fast;
+    // freopen("in.txt", "r", stdin);
+    // freopen("out.txt", "w", stdout);
+    // louisfghbvc
+        solve(1);
+    return 0;
+}
+
+/**
+Enjoy the problem.
+**/
