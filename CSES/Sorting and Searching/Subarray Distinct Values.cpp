2136a36b976da3afeec0a68c26fab7c2507bce67
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-05-23 23:50:42 +0800

    Math, Sliding Window, Two Pointers.

---
+++ CSES/Sorting and Searching/Subarray Distinct Values.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+#define Fast cin.tie(0), ios::sync_with_stdio(0)
+#define All(x) x.begin(), x.end()
+#define louisfghbvc int t; cin >> t; for(int tt = 0; tt < t; ++tt)
+#define sz(x) (int)(x).size()
+#define sort_unique(x) sort(x.begin(), x.end()); x.erase(unique(x.begin(), x.end()));
+using namespace std;
+typedef long long LL;
+typedef pair<LL, LL> ii;
+typedef vector<LL> vi;
+
+template<typename T> istream& operator>>(istream &is, vector<T> &v) { for(auto &it : v) is >> it; return is; }
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '{'; string sep = ""; for(const auto &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+
+void dbg_out() { cerr << " end.\n"; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+const int N = 2e5 + 5;
+const int INF = 0x3f3f3f3f;
+const int mod = 1e9+7;
+/**
+Read problem statement carefully
+5
+3 1 2 7 4
+
+
+**/
+
+void solve(int n){
+    int k;
+    cin >> n >> k;
+    vi arr(n);
+    cin >> arr;
+
+    LL res = 0;
+    map<int, int> mp;
+    int sz = 0;
+    for(int r = 0, l = 0; r < n; ++r){
+        if(++mp[arr[r]] == 1) sz++;
+        while(sz > k){
+            if(--mp[arr[l++]] == 0) sz--;
+        }
+        res += r - l + 1;
+    }
+    cout << res << "\n";
+}
+
+int main()
+{
+    Fast;
+    // freopen("in.txt", "r", stdin);
+    // freopen("out.txt", "w", stdout);
+    // louisfghbvc
+        solve(1);
+    return 0;
+}
+
+/**
+Enjoy the problem.
+**/
