5a8b712409a8c4821445a93e5172c2a396e6bf7a
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-05-27 23:12:20 +0800

    Top down Memo. O(NlogN).

---
+++ CSES/Dynamic Programming/Removing Digits.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+#define Fast cin.tie(0), ios::sync_with_stdio(0)
+#define All(x) x.begin(), x.end()
+#define louisfghbvc int t; cin >> t; for(int tt = 0; tt < t; ++tt)
+#define sz(x) (int)(x).size()
+#define sort_unique(x) sort(x.begin(), x.end()); x.erase(unique(x.begin(), x.end()));
+using namespace std;
+typedef long long LL;
+typedef pair<LL, LL> ii;
+typedef vector<LL> vi;
+
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// template <class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template<typename T> istream& operator>>(istream &is, vector<T> &v) { for(auto &it : v) is >> it; return is; }
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '{'; string sep = ""; for(const auto &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+
+void dbg_out() { cerr << " end.\n"; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+const int N = 1e6 + 5;
+const int INF = 0x3f3f3f3f;
+const int mod = 1e9+7;
+/**
+Read problem statement carefully
+**/
+
+int dp[N];
+int dfs(int n){
+    if(n == 0) return 0;
+    if(dp[n] != -1) return dp[n];
+    int res = 1e9;
+    int tmp = n;
+    while(tmp){
+        int b = tmp%10;
+        if(b) res = min(res, 1+dfs(n-b));
+        tmp /= 10;
+    }
+    return dp[n] = res;
+}
+
+void solve(int T){
+    memset(dp, -1, sizeof dp);
+
+    int n;
+    cin >> n;
+
+    cout << dfs(n) << "\n";
+}
+
+int main()
+{
+    Fast;
+    // freopen("in.txt", "r", stdin);
+    // freopen("out.txt", "w", stdout);
+    // louisfghbvc
+        solve(1);
+    return 0;
+}
+
+/**
+Enjoy the problem.
+**/
