a9136db9a87672f4ddf466c31571a764bb0bc701
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-06-26 00:49:50 +0800

    DFS. sub path record. trick

---
+++ CSES/Graph Algorithms/Round Trip.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+ 
+// #pragma GCC optimize("O3")
+#define Fast cin.tie(0), ios::sync_with_stdio(0)
+#define All(x) x.begin(), x.end()
+#define louisfghbvc int t; cin >> t; for(int tt = 0; tt < t; ++tt)
+#define sz(x) (int)(x).size()
+#define sort_unique(x) sort(x.begin(), x.end()); x.erase(unique(x.begin(), x.end()));
+using namespace std;
+typedef long long LL;
+typedef pair<LL, LL> ii;
+typedef vector<LL> vi;
+ 
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// template <class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+/**
+order_of_key(k) : nums strictly smaller than k
+find_by_order(k): index from 0
+**/
+ 
+template<typename T> istream& operator>>(istream &is, vector<T> &v) { for(auto &it : v) is >> it; return is; }
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '{'; string sep = ""; for(const auto &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+ 
+void dbg_out() { cerr << " end.\n"; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << H << ' '; dbg_out(T...); }
+ 
+const int N = 1e5 + 5;
+const int INF = 0x3f3f3f3f;
+const int mod = 1e9+7;
+/**
+Read problem statement carefully
+4 4
+1 2
+2 3
+3 4
+2 4
+
+**/
+ 
+vi g[N];
+int path[N];
+
+void dfs(int u = 0, int p = -1){
+    for(int v: g[u]){
+        if(v == p) continue;
+        if(path[v]){ // find cycle, do not duplicate use.
+            vi ans;
+            ans.push_back(u);
+            while (ans.back() != v){
+                ans.push_back(path[ans.back()]);
+            }
+            ans.push_back(u);
+            cout << sz(ans) << "\n";
+            for (int x: ans){
+                cout << x << " ";
+            }
+            exit(0);
+        }
+        path[v] = u;
+        dfs(v, u);
+    }
+}
+ 
+void solve(int T){
+    int n, m;
+    cin >> n >> m;
+    for(int i = 0, u, v; i < m; ++i){
+        cin >> u >> v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+ 
+    for(int i = 1; i <= n; ++i){
+        if(!path[i]){
+            path[i] = i;
+            dfs(i);
+        }
+    }
+ 
+    cout << "IMPOSSIBLE\n";
+}   
+ 
+int main()
+{
+    Fast;
+    // freopen("in.txt", "r", stdin);
+    // freopen("out.txt", "w", stdout);
+    // louisfghbvc
+        solve(1);
+    return 0;
+}
+ 
+/**
+Enjoy the problem.
+**/
