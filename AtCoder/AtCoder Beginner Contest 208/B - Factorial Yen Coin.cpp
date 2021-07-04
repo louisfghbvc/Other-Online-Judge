8a4ca0ac6e0bc084285d6c1381b37174279e14ef
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-07-04 22:11:29 +0800

    Greedy.

---
+++ AtCoder/AtCoder Beginner Contest 208/B - Factorial Yen Coin.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+#define Fast cin.tie(0), ios::sync_with_stdio(0)
+#define All(x) x.begin(), x.end()
+#define louisfghbvc int t; cin >> t; for(int tt = 0; tt < t; ++tt)
+#define sz(x) (int)(x).size()
+#define sort_unique(x) sort(x.begin(), x.end()); x.erase(unique(x.begin(), x.end()));
+using namespace std;
+typedef long long LL;
+typedef pair<LL, LL> ii;
+typedef vector<LL> vi;
+
+template<typename T> istream& operator>>(istream &is, vector<T> &v) { for(auto &it : v) is >> it; return is; }
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '{'; string sep = ""; for(const auto &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+
+void dbg_out() { cerr << " end.\n"; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+const int N = 1e5 + 5;
+const int INF = 0x3f3f3f3f;
+const int mod = 1e9+7;
+/**
+Read problem statement carefully
+**/
+
+void solve(int T){
+    vi coins = { 1 };
+    for(int i = 1; i <= 10; ++i)
+        coins.push_back(i * coins.back());
+
+    int p;
+    cin >> p;
+    int cnt = 0;
+    for(int i = sz(coins)-1; i >= 1; --i){
+        for(int k = 0; k < 100; ++k){
+            if(p >= coins[i]) p -= coins[i], cnt++;
+        }
+    }
+
+    cout << cnt << "\n";
+}
+
+int main()
+{
+    Fast;
+    // freopen("in.txt", "r", stdin);
+    // freopen("out.txt", "w", stdout);
+    // louisfghbvc
+        solve(1);
+    return 0;
+}
+
+/**
+Enjoy the problem.
+**/
