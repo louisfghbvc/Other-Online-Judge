bed57ad19ac708289870e50559196c6d597eb8a5
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-07-31 18:01:58 +0800

    Dijkstra tweak.

---
+++ CSES/Graph Algorithms/Flight Discount.cpp
@@ -0,0 +1,92 @@
+/**                                          
+ __         ______     __  __     __     ______     ______   ______     __  __     ______     __   __   ______    
+/\ \       /\  __ \   /\ \/\ \   /\ \   /\  ___\   /\  ___\ /\  ___\   /\ \_\ \   /\  == \   /\ \ / /  /\  ___\   
+\ \ \____  \ \ \/\ \  \ \ \_\ \  \ \ \  \ \___  \  \ \  __\ \ \ \__ \  \ \  __ \  \ \  __<   \ \ \'/   \ \ \____  
+ \ \_____\  \ \_____\  \ \_____\  \ \_\  \/\_____\  \ \_\    \ \_____\  \ \_\ \_\  \ \_____\  \ \__|    \ \_____\ 
+  \/_____/   \/_____/   \/_____/   \/_/   \/_____/   \/_/     \/_____/   \/_/\/_/   \/_____/   \/_/      \/_____/                                       
+**/
+#include <bits/stdc++.h>
+// #pragma GCC optimize("O3")
+#define Fast cin.tie(0), ios::sync_with_stdio(0)
+#define All(x) x.begin(), x.end()
+#define louisfghbvc int t; cin >> t; for(int tt = 0; tt < t; ++tt)
+#define sz(x) (int)(x).size()
+#define sort_unique(x) sort(All(x)); x.erase(unique(All(x)), x.end());
+using namespace std;
+typedef long long LL;
+typedef pair<LL, LL> ii;
+typedef vector<LL> vi;
+ 
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// template <class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+/**
+order_of_key(k) : nums strictly smaller than k
+find_by_order(k): index from 0
+**/
+ 
+template<typename T> istream& operator>>(istream &is, vector<T> &v) { for(auto &it : v) is >> it; return is; }
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '{'; string sep = ""; for(const auto &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+ 
+void dbg_out() { cerr << " end.\n"; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << H << ' '; dbg_out(T...); }
+ 
+const int N = 1e5+5;
+const int INF = 0x3f3f3f3f;
+const int mod = 1e9+7;
+/**
+**/
+
+vector<ii> g[N];
+
+void solve(int T){
+    int n, m;
+    cin >> n >> m;
+    for(int i = 0; i < m; ++i){
+        int a, b, c;
+        cin >> a >> b >> c;
+        g[a].push_back({b, c});
+    }
+
+    vector<vi> dp(n+1, vi(2, LLONG_MAX));
+    priority_queue<array<LL, 3>, vector<array<LL, 3>>, greater<array<LL, 3>>> pq; // cost, node, use coupon or not
+    
+    dp[1][0] = dp[1][1] = 0;
+    pq.push({dp[1][0], 1, 0});
+    while(pq.size()){
+        auto [cost, u, used] = pq.top(); pq.pop();
+        if(cost > dp[u][used]) continue;
+        for(auto &[v, w]: g[u]){
+            if(!used){
+                if(dp[v][1] > dp[u][0] + w/2){
+                    dp[v][1] = dp[u][0] + w/2;
+                    pq.push({dp[v][1], v, 1});
+                }
+                if(dp[v][0] > dp[u][0] + w){
+                    dp[v][0] = dp[u][0] + w;
+                    pq.push({dp[v][0], v, 0});
+                }
+            }
+            else{
+                if(dp[v][1] > dp[u][1] + w){
+                    dp[v][1] = dp[u][1] + w;
+                    pq.push({dp[v][1], v, 1});
+                }
+            }
+        }
+    }
+
+    cout << min(dp[n][1], dp[n][0]) << "\n";
+}   
+ 
+int main()
+{
+    Fast;
+    // freopen("in.txt", "r", stdin);
+    // freopen("out.txt", "w", stdout);
+    // louisfghbvc
+        solve(1);
+    return 0;
+}
