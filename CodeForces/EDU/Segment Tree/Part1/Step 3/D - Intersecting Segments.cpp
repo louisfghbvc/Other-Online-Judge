5eecbd8ac837219778be321f43b8de0c4a3c00cd
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-10-16 14:04:33 +0800

    intersect segment, 2 pass.

---
+++ CodeForces/EDU/Segment Tree/Part1/Step 3/D - Intersecting Segments.cpp
@@ -0,0 +1,113 @@
+/**                                          
+ __         ______     __  __     __     ______     ______   ______     __  __     ______     __   __   ______    
+/\ \       /\  __ \   /\ \/\ \   /\ \   /\  ___\   /\  ___\ /\  ___\   /\ \_\ \   /\  == \   /\ \ / /  /\  ___\   
+\ \ \____  \ \ \/\ \  \ \ \_\ \  \ \ \  \ \___  \  \ \  __\ \ \ \__ \  \ \  __ \  \ \  __<   \ \ \'/   \ \ \____  
+ \ \_____\  \ \_____\  \ \_____\  \ \_\  \/\_____\  \ \_\    \ \_____\  \ \_\ \_\  \ \_____\  \ \__|    \ \_____\ 
+  \/_____/   \/_____/   \/_____/   \/_/   \/_____/   \/_/     \/_____/   \/_/\/_/   \/_____/   \/_/      \/_____/                                       
+**/
+#include <bits/stdc++.h>
+// #pragma GCC optimize("O3")
+#define Fast cin.tie(0), ios::sync_with_stdio(0)
+#define All(x) x.begin(), x.end()
+#define louisfghbvc int t; cin >> t; for(int tt = 0; tt < t; ++tt)
+#define sz(x) (int)(x).size()
+#define sort_unique(x) sort(All(x)); x.erase(unique(All(x)), x.end());
+using namespace std;
+typedef long long LL;
+typedef pair<LL, LL> ii;
+typedef vector<LL> vi;
+ 
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// template <class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+/**
+order_of_key(k) : nums strictly smaller than k
+find_by_order(k): index from 0
+**/
+ 
+template<typename T> istream& operator>>(istream &is, vector<T> &v) { for(auto &it : v) is >> it; return is; }
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '{'; string sep = ""; for(const auto &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+ 
+void dbg_out() { cerr << " end.\n"; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << H << ' '; dbg_out(T...); }
+ 
+const int N = 1e5+5;
+const int INF = 0x3f3f3f3f;
+const int mod = 1e9+7;
+/**
+**/
+
+int tre[8*N];
+int n;
+
+class SegmentTree{
+public:
+    SegmentTree() {}
+    void update(int i, int v, int id=1, int l=0, int r=2*n-1){
+        if(l == i && i == r){
+            tre[id] += v; return;
+        }
+        int mid = (l+r)/2;
+        if(i <= mid) update(i, v, id*2, l, mid);
+        else update(i, v, id*2+1, mid+1, r);
+        tre[id] = tre[id*2] + tre[id*2+1];
+    }
+    int query(int ll, int rr, int id=1, int l=0, int r=2*n-1){
+        if(r < ll || l > rr) return 0;
+        if(l >= ll && r <= rr) return tre[id];
+        int mid = (l+r)/2;
+        return query(ll, rr, id*2, l, mid) + query(ll, rr, id*2+1, mid+1, r);
+    }
+};
+
+void solve(int T){
+    cin >> n;
+
+    vi arr(2*n);
+    cin >> arr;
+
+    SegmentTree st;
+
+    vi vis(n, -1); // record the index, [l,r] cover left point
+    vi ans(n);
+    for(int i = 0; i < 2*n; ++i){
+        int val = arr[i]-1;
+        if(vis[val] != -1){
+            st.update(vis[val], -1);
+            ans[val] = st.query(vis[val], i);
+        }
+        else{
+            st.update(i, 1);
+            vis[val] = i;
+        }
+    }
+
+    // reverse part
+    vis.assign(n, -1);
+    for(int i = 0; i < 2*n; ++i){
+        int val = arr[2*n-1-i]-1;
+        if(vis[val] != -1){
+            st.update(vis[val], -1);
+            ans[val] += st.query(vis[val], i);
+        }
+        else{
+            st.update(i, 1);
+            vis[val] = i;
+        }
+    }
+    
+    for(int i = 0; i < n; ++i)
+        cout << ans[i] << " ";
+}   
+ 
+int main()
+{
+    Fast;
+    // freopen("in.txt", "r", stdin);
+    // freopen("out.txt", "w", stdout);
+    // louisfghbvc
+        solve(1);
+    return 0;
+}
