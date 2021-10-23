f3ac4a82485c96a3eb1d509b3b25343f941e67d7
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-10-23 21:54:21 +0800

    use formula.

---
+++ AtCoder/AtCoder Beginner Contest 224/C - Triangle.cpp
@@ -0,0 +1,87 @@
+/**                                          
+ __         ______     __  __     __     ______     ______   ______     __  __     ______     __   __   ______    
+/\ \       /\  __ \   /\ \/\ \   /\ \   /\  ___\   /\  ___\ /\  ___\   /\ \_\ \   /\  == \   /\ \ / /  /\  ___\   
+\ \ \____  \ \ \/\ \  \ \ \_\ \  \ \ \  \ \___  \  \ \  __\ \ \ \__ \  \ \  __ \  \ \  __<   \ \ \'/   \ \ \____  
+ \ \_____\  \ \_____\  \ \_____\  \ \_\  \/\_____\  \ \_\    \ \_____\  \ \_\ \_\  \ \_____\  \ \__|    \ \_____\ 
+  \/_____/   \/_____/   \/_____/   \/_/   \/_____/   \/_/     \/_____/   \/_/\/_/   \/_____/   \/_/      \/_____/                                       
+**/
+#include <bits/stdc++.h>
+// #pragma GCC optimize("O3")
+#define Fast cin.tie(0), ios::sync_with_stdio(0)
+#define All(x) x.begin(), x.end()
+#define louisfghbvc int t; cin >> t; for(int tt = 0; tt < t; ++tt)
+#define sz(x) (int)(x).size()
+#define sort_unique(x) sort(All(x)); x.erase(unique(All(x)), x.end());
+using namespace std;
+typedef long long LL;
+typedef pair<LL, LL> ii;
+typedef vector<LL> vi;
+ 
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// template <class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+/**
+order_of_key(k) : nums strictly smaller than k
+find_by_order(k): index from 0
+**/
+ 
+template<typename T> istream& operator>>(istream &is, vector<T> &v) { for(auto &it : v) is >> it; return is; }
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '{'; string sep = ""; for(const auto &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+ 
+void dbg_out() { cerr << " end.\n"; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << H << ' '; dbg_out(T...); }
+ 
+const int N = 2e5+5;
+const int INF = 0x3f3f3f3f;
+const int mod = 1e9+7;
+/**
+ * 
+4
+987654321 987654321
+314159265 358979323
+-123456789 123456789
+-1000000000 1000000000
+
+**/
+
+LL dis(ii &a, ii&b){
+    return (a.first-b.first)*(a.first-b.first) + (a.second-b.second)*(a.second-b.second);
+}
+
+// fuck use formula.
+// http://www2.chsh.chc.edu.tw/bee/108highschool/1080309.pdf
+// trick
+bool triangle(ii &p1, ii &p2, ii &p3){
+    return (p2.first-p1.first)*(p3.second-p1.second) - 
+        (p3.first-p1.first)*(p2.second-p1.second) != 0;
+}
+
+void solve(int T){
+    int n;
+    cin >> n;
+    vector<ii> arr(n);
+    for(auto &it: arr)
+        cin >> it.first >> it.second;
+
+    int res = 0;
+    for(int i = 0; i < n; ++i){
+        for(int j = i+1; j < n; ++j){
+            for(int k = j+1; k < n; ++k){
+                res += triangle(arr[i], arr[j], arr[k]);
+            }
+        }
+    }
+    cout << res << "\n";
+}   
+ 
+int main()
+{
+    Fast;
+    // freopen("in.txt", "r", stdin);
+    // freopen("out.txt", "w", stdout);
+    // louisfghbvc
+        solve(1);
+    return 0;
+}
