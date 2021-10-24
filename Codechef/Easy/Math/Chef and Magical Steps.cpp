779d8e94045ad1566295684d99112aedc051fec9
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-10-24 22:53:46 +0800

    Prefix sum, Sieve.

---
+++ Codechef/Easy/Math/Chef and Magical Steps.cpp
@@ -0,0 +1,72 @@
+/**                                          
+ __         ______     __  __     __     ______     ______   ______     __  __     ______     __   __   ______    
+/\ \       /\  __ \   /\ \/\ \   /\ \   /\  ___\   /\  ___\ /\  ___\   /\ \_\ \   /\  == \   /\ \ / /  /\  ___\   
+\ \ \____  \ \ \/\ \  \ \ \_\ \  \ \ \  \ \___  \  \ \  __\ \ \ \__ \  \ \  __ \  \ \  __<   \ \ \'/   \ \ \____  
+ \ \_____\  \ \_____\  \ \_____\  \ \_\  \/\_____\  \ \_\    \ \_____\  \ \_\ \_\  \ \_____\  \ \__|    \ \_____\ 
+  \/_____/   \/_____/   \/_____/   \/_/   \/_____/   \/_/     \/_____/   \/_/\/_/   \/_____/   \/_/      \/_____/                                       
+**/
+#include <bits/stdc++.h>
+// #pragma GCC optimize("O3")
+#define Fast cin.tie(0), ios::sync_with_stdio(0)
+#define All(x) x.begin(), x.end()
+#define louisfghbvc int t; cin >> t; for(int tt = 0; tt < t; ++tt)
+#define sz(x) (int)(x).size()
+#define sort_unique(x) sort(All(x)); x.erase(unique(All(x)), x.end());
+using namespace std;
+typedef long long LL;
+typedef pair<LL, LL> ii;
+typedef vector<LL> vi;
+ 
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// template <class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+/**
+order_of_key(k) : nums strictly smaller than k
+find_by_order(k): index from 0
+**/
+ 
+template<typename T> istream& operator>>(istream &is, vector<T> &v) { for(auto &it : v) is >> it; return is; }
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '{'; string sep = ""; for(const auto &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+ 
+void dbg_out() { cerr << " end.\n"; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << H << ' '; dbg_out(T...); }
+ 
+const int N = 1e7+5;
+const int INF = 0x3f3f3f3f;
+const int mod = 1e9+7;
+/**
+**/
+
+bool np[N];
+int prime_num[N];
+
+void sieve(){
+    for(int i = 2; i < N; ++i){
+        prime_num[i] = prime_num[i-1];
+        if(!np[i]){
+            prime_num[i]++;
+            for(int j = 2*i; j < N; j+=i)
+                np[j] = 1;
+        }
+    }
+}
+
+void solve(int T){
+    int x, y;
+    cin >> x >> y;
+    // prefix sum, cal prime number [x+2, y]
+    cout << y - x - (prime_num[y] - prime_num[x+1]) << "\n";
+}   
+ 
+int main()
+{
+    Fast;
+    sieve();
+    // freopen("in.txt", "r", stdin);
+    // freopen("out.txt", "w", stdout);
+    louisfghbvc
+        solve(1);
+    return 0;
+}
