8a7df29c508a2a190715d02d0a293cde911b4853
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-11-14 23:34:56 +0800

    check grap, carefully

---
+++ AtCoder/AtCoder Beginner Contest 226/E - Just one.cpp
@@ -0,0 +1,100 @@
+/**                                          
+ __         ______     __  __     __     ______     ______   ______     __  __     ______     __   __   ______    
+/\ \       /\  __ \   /\ \/\ \   /\ \   /\  ___\   /\  ___\ /\  ___\   /\ \_\ \   /\  == \   /\ \ / /  /\  ___\   
+\ \ \____  \ \ \/\ \  \ \ \_\ \  \ \ \  \ \___  \  \ \  __\ \ \ \__ \  \ \  __ \  \ \  __<   \ \ \'/   \ \ \____  
+ \ \_____\  \ \_____\  \ \_____\  \ \_\  \/\_____\  \ \_\    \ \_____\  \ \_\ \_\  \ \_____\  \ \__|    \ \_____\ 
+  \/_____/   \/_____/   \/_____/   \/_/   \/_____/   \/_/     \/_____/   \/_/\/_/   \/_____/   \/_/      \/_____/                                       
+**/
+#include <bits/stdc++.h>
+// #pragma GCC optimize("O3")
+#define Fast cin.tie(0), ios::sync_with_stdio(0)
+#define All(x) x.begin(), x.end()
+#define louisfghbvc int t; cin >> t; for(int tt = 0; tt < t; ++tt)
+#define sz(x) (int)(x).size()
+#define sort_unique(x) sort(All(x)); x.erase(unique(All(x)), x.end());
+using namespace std;
+typedef long long LL;
+typedef pair<LL, LL> ii;
+typedef vector<LL> vi;
+ 
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// template <class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+/**
+order_of_key(k) : nums strictly smaller than k
+find_by_order(k): index from 0
+**/
+ 
+template<typename T> istream& operator>>(istream &is, vector<T> &v) { for(auto &it : v) is >> it; return is; }
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '{'; string sep = ""; for(const auto &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+ 
+void dbg_out() { cerr << " end.\n"; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << H << ' '; dbg_out(T...); }
+ 
+const int N = 2e5+5;
+const int INF = 0x3f3f3f3f;
+const int mod = 998244353;
+/**
+ * 
+6 6 
+1 2
+3 4
+3 5
+4 5
+4 6
+5 6
+
+**/
+
+vi g[N];
+bool vis[N];
+int x, y;
+
+void dfs(int u){
+    x++;
+    y += g[u].size();
+    for(auto& v: g[u]){
+        if(!vis[v]){
+            vis[v] = 1;
+            dfs(v);
+        }
+    }
+}
+
+void solve(int T){
+    int n, m;
+    cin >> n >> m;
+
+    for(int i = 0; i < m; ++i){
+        int u, v;
+        cin >> u >> v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+
+    LL res = 1;
+    // each connect group have 2 choose
+    for(int i = 1; i <= n; ++i){
+        if(!vis[i]){
+            x = y = 0;
+            vis[i] = 1;
+            dfs(i);
+            if(2*x != y) res = 0;
+            else res = res * 2 % mod;
+        }
+    }
+    
+    cout << res << "\n";
+}   
+ 
+int main()
+{
+    Fast;
+    // freopen("in.txt", "r", stdin);
+    // freopen("out.txt", "w", stdout);
+    // louisfghbvc
+        solve(1);
+    return 0;
+}
