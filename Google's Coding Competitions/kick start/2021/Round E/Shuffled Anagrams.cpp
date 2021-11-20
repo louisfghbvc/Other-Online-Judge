d460ab53a99d2d5ec5bdb7abce997a224f1ebabf
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2021-11-20 17:29:48 +0800

    Construct algorithm

---
+++ Google's Coding Competitions/kick start/2021/Round E/Shuffled Anagrams.cpp
@@ -0,0 +1,81 @@
+/**                                          
+ __         ______     __  __     __     ______     ______   ______     __  __     ______     __   __   ______    
+/\ \       /\  __ \   /\ \/\ \   /\ \   /\  ___\   /\  ___\ /\  ___\   /\ \_\ \   /\  == \   /\ \ / /  /\  ___\   
+\ \ \____  \ \ \/\ \  \ \ \_\ \  \ \ \  \ \___  \  \ \  __\ \ \ \__ \  \ \  __ \  \ \  __<   \ \ \'/   \ \ \____  
+ \ \_____\  \ \_____\  \ \_____\  \ \_\  \/\_____\  \ \_\    \ \_____\  \ \_\ \_\  \ \_____\  \ \__|    \ \_____\ 
+  \/_____/   \/_____/   \/_____/   \/_/   \/_____/   \/_/     \/_____/   \/_/\/_/   \/_____/   \/_/      \/_____/                                       
+**/
+#include <bits/stdc++.h>
+// #pragma GCC optimize("O3")
+#define Fast cin.tie(0), ios::sync_with_stdio(0)
+#define All(x) x.begin(), x.end()
+#define louisfghbvc int t; cin >> t; for(int tt = 0; tt < t; ++tt)
+#define sz(x) (int)(x).size()
+#define sort_unique(x) sort(All(x)); x.erase(unique(All(x)), x.end());
+using namespace std;
+typedef long long LL;
+typedef pair<LL, LL> ii;
+typedef vector<LL> vi;
+ 
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// template <class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+/**
+order_of_key(k) : nums strictly smaller than k
+find_by_order(k): index from 0
+**/
+ 
+template<typename T> istream& operator>>(istream &is, vector<T> &v) { for(auto &it : v) is >> it; return is; }
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '{'; string sep = ""; for(const auto &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+ 
+void dbg_out() { cerr << " end.\n"; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << H << ' '; dbg_out(T...); }
+ 
+const int N = 2e5+5;
+const int INF = 0x3f3f3f3f;
+const int mod = 1e9+7;
+/**
+**/
+
+void solve(int T){
+    printf("Case #%d: ", ++T);
+
+    string s;
+    cin >> s;
+    int n = s.size();
+
+    // record each chars index
+    vector<int> ind(26);
+    for(int i = 0; i < n; ++i)
+        ind[s[i]-'a']++;
+    // frequency > half
+    for(int i = 0; i < 26; ++i){
+        if(ind[i] > n/2){
+            cout << "IMPOSSIBLE\n";
+            return;
+        }
+    }
+
+    vector<pair<char, int>> arr;
+    for(int i = 0; i < n; ++i)
+        arr.push_back({s[i], i});
+    sort(All(arr));
+    
+    string t(n, 'a'); // cycle shift put it.
+    for(int i = 0; i < n; ++i)
+        t[arr[(i + n/2)%n].second] = arr[i].first;
+    
+    cout << t << "\n";
+}   
+ 
+int main()
+{
+    // Fast;
+    // freopen("in.txt", "r", stdin);
+    // freopen("out.txt", "w", stdout);
+    louisfghbvc
+        solve(tt);
+    return 0;
+}
