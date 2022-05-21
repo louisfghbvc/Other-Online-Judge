195f95c207ec5e5aad3483a93b8ee07c1fdb2206
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2022-05-21 21:55:51 +0800

    Greedy, merge two,

---
+++ AtCoder/AtCoder Beginner Contest 252/F - Bread.cpp
@@ -0,0 +1,76 @@
+/**
+ __         ______     __  __     __     ______     ______   ______     __  __     ______     __   __   ______
+/\ \       /\  __ \   /\ \/\ \   /\ \   /\  ___\   /\  ___\ /\  ___\   /\ \_\ \   /\  == \   /\ \ / /  /\  ___\
+\ \ \____  \ \ \/\ \  \ \ \_\ \  \ \ \  \ \___  \  \ \  __\ \ \ \__ \  \ \  __ \  \ \  __<   \ \ \'/   \ \ \____
+ \ \_____\  \ \_____\  \ \_____\  \ \_\  \/\_____\  \ \_\    \ \_____\  \ \_\ \_\  \ \_____\  \ \__|    \ \_____\
+  \/_____/   \/_____/   \/_____/   \/_/   \/_____/   \/_/     \/_____/   \/_/\/_/   \/_____/   \/_/      \/_____/
+**/
+#include <bits/stdc++.h>
+// #pragma GCC optimize("O3")
+#define Fast cin.tie(0), ios::sync_with_stdio(0)
+#define All(x) x.begin(), x.end()
+#define louisfghbvc int t; cin >> t; for(int tt = 0; tt < t; ++tt)
+#define sz(x) (int)(x).size()
+#define sort_unique(x) sort(All(x)); x.erase(unique(All(x)), x.end());
+using namespace std;
+typedef long long LL;
+typedef pair<LL, LL> ii;
+typedef vector<LL> vi;
+
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// template <class T> using ordered_set = tree<T, null_type, greater<T>, rb_tree_tag, tree_order_statistics_node_update>;
+/**
+order_of_key(k) : nums strictly smaller than k
+find_by_order(k): index from 0
+**/
+
+template<typename T> istream& operator>>(istream &is, vector<T> &v) { for(auto &it : v) is >> it; return is; }
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '{'; string sep = ""; for(const auto &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+
+void dbg_out() { cerr << " end.\n"; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << H << ' '; dbg_out(T...); }
+
+const int N = 1e3+5;
+const LL INF = 1e13;
+const int mod = 998244353;
+const LL MXP = 1e10;
+/**
+**/
+
+void solve(int T) {
+    // cout << "Case #" << T+1 << ": ";
+    LL n, l;
+    cin >> n >> l;
+
+    vi arr(n);
+    cin >> arr;
+
+    // goal: give the children exactly ai
+    // always choose two smallest segment, merge them
+    // a edge case if l still remain, we should add l to heap
+
+    priority_queue<LL, vi, greater<>> min_heap(All(arr));
+    l -= accumulate(All(arr), 0LL);
+    if (l) min_heap.push(l); // l also can use to other
+
+    LL ans = 0;
+    while (min_heap.size() > 1) {
+        LL a = min_heap.top(); min_heap.pop();
+        LL b = min_heap.top(); min_heap.pop();
+        ans += a+b;
+        min_heap.push(a+b);
+    }
+
+    cout << ans << "\n";
+}
+
+int main(){
+    Fast;
+    // freopen("in.txt", "r", stdin), freopen("out.txt", "w", stdout);
+    // louisfghbvc
+        solve(1);
+    return 0;
+}
