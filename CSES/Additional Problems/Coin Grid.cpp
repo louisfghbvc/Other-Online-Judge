df3a54c7c8bf033fb5fe0be3dbeb0f7a2ac5a7d4
尾玉 <43661591+louisfghbvc@users.noreply.github.com>
2024-11-03 13:47:21 +0800

    max flow min cut, find cut edge

---
+++ CSES/Additional Problems/Coin Grid.cpp
@@ -0,0 +1,192 @@
+/**                                          
+ __         ______     __  __     __     ______     ______   ______     __  __     ______     __   __   ______    
+/\ \       /\  __ \   /\ \/\ \   /\ \   /\  ___\   /\  ___\ /\  ___\   /\ \_\ \   /\  == \   /\ \ / /  /\  ___\   
+\ \ \____  \ \ \/\ \  \ \ \_\ \  \ \ \  \ \___  \  \ \  __\ \ \ \__ \  \ \  __ \  \ \  __<   \ \ \'/   \ \ \____  
+ \ \_____\  \ \_____\  \ \_____\  \ \_\  \/\_____\  \ \_\    \ \_____\  \ \_\ \_\  \ \_____\  \ \__|    \ \_____\ 
+  \/_____/   \/_____/   \/_____/   \/_/   \/_____/   \/_/     \/_____/   \/_/\/_/   \/_____/   \/_/      \/_____/                                       
+**/
+#include <bits/stdc++.h>
+// #pragma GCC optimize("O3")
+#define Fast cin.tie(0), ios::sync_with_stdio(0)
+#define All(x) x.begin(), x.end()
+#define louisfghbvc int t; cin >> t; for(int tt = 0; tt < t; ++tt)
+#define sz(x) (int)(x).size()
+#define sort_unique(x) sort(All(x)); x.erase(unique(All(x)), x.end());
+using namespace std;
+typedef long long LL;
+typedef pair<LL, LL> ii;
+typedef vector<LL> vi;
+ 
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// template <class T> using ordered_set = tree<T, null_type, greater<T>, rb_tree_tag, tree_order_statistics_node_update>;
+/**
+order_of_key(k) : nums strictly smaller than k
+find_by_order(k): index from 0
+**/
+ 
+template<typename T> istream& operator>>(istream &is, vector<T> &v) { for(auto &it : v) is >> it; return is; }
+template<typename T> ostream& operator<<(ostream &os, const vector<T> &v) { os << '{'; string sep = ""; for(const auto &x : v) os << sep << x, sep = ", "; return os << '}'; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+
+void dbg_out() { cout << " end.\n"; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << H << ' '; dbg_out(T...); }
+ 
+const int N = 1e5+5;
+const LL INF = 1e15;
+const int mod = 1e9+7;
+const LL MXP = 1e10;
+const int dir[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+/**
+**/
+
+template<class T>
+struct MaxFlow {
+    struct _Edge {
+        int to;
+        T cap;
+        _Edge(int to, T cap) : to(to), cap(cap) {}
+    };
+    int n;
+    std::vector<_Edge> e;
+    std::vector<std::vector<int>> g;
+    std::vector<int> cur, h;
+    MaxFlow() {}
+    MaxFlow(int n) {
+        init(n);
+    }
+    void init(int n) {
+        this->n = n;
+        e.clear();
+        g.assign(n, {});
+        cur.resize(n);
+        h.resize(n);
+    }
+    bool bfs(int s, int t) {
+        h.assign(n, -1);
+        std::queue<int> que;
+        h[s] = 0;
+        que.push(s);
+        while (!que.empty()) {
+            const int u = que.front();
+            que.pop();
+            for (int i : g[u]) {
+                auto [v, c] = e[i];
+                if (c > 0 && h[v] == -1) {
+                    h[v] = h[u] + 1;
+                    if (v == t) {
+                        return true;
+                    }
+                    que.push(v);
+                }
+            }
+        }
+        return false;
+    }
+    T dfs(int u, int t, T f) {
+        if (u == t) {
+            return f;
+        }
+        auto r = f;
+        for (int &i = cur[u]; i < int(g[u].size()); ++i) {
+            const int j = g[u][i];
+            auto [v, c] = e[j];
+            if (c > 0 && h[v] == h[u] + 1) {
+                auto a = dfs(v, t, std::min(r, c));
+                e[j].cap -= a;
+                e[j ^ 1].cap += a;
+                r -= a;
+                if (r == 0) {
+                    return f;
+                }
+            }
+        }
+        return f - r;
+    }
+    void addEdge(int u, int v, T c) {
+        g[u].push_back(e.size());
+        e.emplace_back(v, c);
+        g[v].push_back(e.size());
+        e.emplace_back(u, 0);
+    }
+    T flow(int s, int t) {
+        T ans = 0;
+        while (bfs(s, t)) {
+            cur.assign(n, 0);
+            ans += dfs(s, t, std::numeric_limits<T>::max());
+        }
+        return ans;
+    }
+    std::vector<bool> minCut() {
+        std::vector<bool> c(n);
+        for (int i = 0; i < n; i++) {
+            c[i] = (h[i] != -1);
+        }
+        return c;
+    }
+    struct Edge {
+        int from;
+        int to;
+        T cap;
+        T flow;
+    };
+    std::vector<Edge> edges() {
+        std::vector<Edge> a;
+        for (int i = 0; i < e.size(); i += 2) {
+            Edge x;
+            x.from = e[i + 1].to;
+            x.to = e[i].to;
+            x.cap = e[i].cap + e[i + 1].cap;
+            x.flow = e[i + 1].cap;
+            a.push_back(x);
+        }
+        return a;
+    }
+};
+
+void solve(int gg) {
+    // cout << "Case #" << T+1 << ": ";
+    
+    // goal: find the minimum move to cover all 'o'
+    
+    // idea:
+    // minimum node cover in bipartite graph
+    // use max flow, to find the min cut
+    
+    int n; 
+    cin >> n;
+    
+    vector<string> grid(n);
+    cin >> grid;
+    
+    MaxFlow<int> g(n + n + 2);
+    int s = n*2, t = s + 1;
+    
+    for (int i = 0; i < n; ++i) {
+        g.addEdge(s, i, 1);        
+        g.addEdge(n+i, t, 1);
+        for (int j = 0; j < n; ++j) {
+            if (grid[i][j] == 'o') {
+                g.addEdge(i, n + j, (int)1e9);
+            }
+        }
+    }
+    
+    cout << g.flow(s, t) << '\n';
+    for (int i = 0; i < n; ++i) if (g.h[i] == -1) {
+        cout << 1 << ' ' << i+1 << '\n';        
+    }
+    for (int i = 0; i < n; ++i) if (g.h[n+i] != -1) {
+        cout << 2 << ' ' << i+1 << '\n';        
+    }
+}       
+
+int main(){
+    Fast;
+    // freopen("in.txt", "r", stdin), freopen("out.txt", "w", stdout);
+    // louisfghbvc
+        solve(1);
+    return 0;
+}
+ 
